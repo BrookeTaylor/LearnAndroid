eecb327a8996393273a1e78d11767b832436c0dd
Brooke Taylor <anonyoumiss@gmail.com>
2025-12-06 21:14:31 -0600

    Exploring how Material Design in Jetpack Compose is used to theme an app.

---
+++ Week_04/Material_Design/README.md
@@ -0,0 +1,7 @@
+Title: How to Build Android Applications with Kotlin  
+Author: Alex Forrester  
+Date: 12/06/2025  
+Description: Pages 32-44. Exercise 1.04 - Exploring how Material Design in Jetpack Compose is used to theme an app. 
+
+Also, created an icon / launch screen. 
+
---
+++ Week_04/Material_Design/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,62 @@
+/*
+Android Studying
+Title: How to Build Android Applications with Kotlin
+Author: Alex Forrester
+Date: 12/06/2025
+Description: Pages 32-.
+ */
+
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+// importing material theme 3
+import androidx.compose.material3.MaterialTheme
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        // Setting display to large
+        style = MaterialTheme.typography.displayLarge,
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
