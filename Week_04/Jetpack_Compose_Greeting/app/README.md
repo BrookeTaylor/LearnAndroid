70748dd7f273f1dcd7194de3ccf9e3bdbd49d572
Brooke Taylor <anonyoumiss@gmail.com>
2025-12-12 11:43:27 -0600

    Finished Project 1.05

--- Week_04/Jetpack_Compose_Greeting/app/README.md
+++ Week_04/Jetpack_Compose_Greeting/app/README.md
@@ -1,6 +1,6 @@
 Title: Exercise 1.05 - Building a Jetpack Compose UI to display a bespoke greeting to the user  
 Author: Alex Forrester  
-Date: 12/11/2025  
+Date: 12/12/2025  
 Description: Learning Jetpack Compose.  
 
 ---
@@ -15,4 +15,10 @@ Description: Learning Jetpack Compose.
 
 5. Add the string resources. In Compose, in order to access a string resource, you use the `stringResource` composable, passing in the ID of the string you want to retrieve from the `strings.xml` file.
 
-6. 
+6. Next, add a layout composable to arrange how we add the content. Add a Column layout composable to display the content in a column.
+
+7. Add two `OutlinedTextField` composables for the first/last name in the `Column` layout.
+
+8. Now, add the interaction to display a welcome message with validation of the text input. Add a button with behavior to display the user's full name and welcome them to the app, show an error if either of the field composables are empty.
+
+
--- Week_04/Jetpack_Compose_Greeting/app/src/main/java/com/example/jetpack_compose_greeting/MainActivity.kt
+++ Week_04/Jetpack_Compose_Greeting/app/src/main/java/com/example/jetpack_compose_greeting/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+// import androidx.compose.foundation.layout.fillMaxSize
+// import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,6 +30,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
+// fix
+import android.widget.Toast
+import android.view.Gravity
+
+
 
 
 class MainActivity : ComponentActivity() {
@@ -40,16 +45,89 @@ class MainActivity : ComponentActivity() {
             Jetpack_Compose_GreetingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
+
                     // Step 4. Create properties to store first/last/full name.
                     var firstName by remember { mutableStateOf("") }
                     var lastName by remember { mutableStateOf("") }
                     var fullName by remember { mutableStateOf("") }
+
 
                     // Step 5. Add the two string resources.
                     var welcomeMessage =
                         stringResource(id = R.string.welcome_to_the_app)
                     var enterNameError =
                         stringResource(id = R.string.please_enter_a_name)
+
+
+                    // Step 6. Add a Column layout composable to display the content in a column.
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .padding(12.dp),
+                        horizontalAlignment =
+                            Alignment.CenterHorizontally,
+                        verticalArrangement =
+                            Arrangement.spacedBy(16.dp)
+                    ) {
+
+                        // Step 7. Add two OutlinedTextField composables for first/last name.
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = firstName,
+                            onValueChange = { firstName = it },
+                            label = {
+                                Text(
+                                    text = stringResource(
+                                        id = R.string.first_name
+                                    )
+                                )
+                            },
+                        )
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = lastName,
+                            onValueChange = { lastName = it },
+                            label = {
+                                Text(
+                                    text = stringResource(
+                                        id = R.string.last_name
+                                    )
+                                )
+                            }
+                        )
+
+                        // Step 8. Add the interaction to display a welcome message with validation.
+                        val context = LocalContext.current
+                        Button(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                if (
+                                    firstName.isNotBlank() &&
+                                    lastName.isNotBlank())
+                                    fullName = "$firstName $lastName"
+                            else {
+                                fullName = ""
+                                    val toast = Toast.makeText(
+                                        context,
+                                        enterNameError,
+                                        Toast.LENGTH_LONG
+                                    )
+                                    toast.setGravity(Gravity.CENTER, 0,0)
+                                    toast.show()
+                                }
+                            }
+                        ) {
+                            Text("Enter")
+                        }
+                        if (fullName.isNotBlank()) {
+                            Text(text = "$welcomeMessage $fullName!")
+                        }
+
+
+
+                    }
+
 
                 }
             }
