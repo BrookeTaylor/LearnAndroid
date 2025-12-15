12f6438047df288bf69dbb6f5dfffcb7752d0f10
Brooke Taylor <anonyoumiss@gmail.com>
2025-12-14 18:54:12 -0600

    Finished Activity 1.01

--- Week_04/RGBApp/app/src/main/java/com/example/rgbapp/MainActivity.kt
+++ Week_04/RGBApp/app/src/main/java/com/example/rgbapp/MainActivity.kt
@@ -15,6 +15,21 @@ import com.example.rgbapp.ui.theme.RGBAppTheme
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material3.Button
+
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.background
+import androidx.core.graphics.toColorInt
+
+
+
 
 
 class MainActivity : ComponentActivity() {
@@ -25,20 +40,113 @@ class MainActivity : ComponentActivity() {
             RGBAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-                    // step 2. add a title Text composable
-                    Text(
+
+                    Column(
                         modifier = Modifier
                             .padding(innerPadding)
-                            .padding(horizontal = 16.dp),
-                        text = "Add two hexadecimal characters between 0-9, A-F or a-f without the '#' for each channel"
-                    )
+                            .padding(horizontal = 16.dp)
+                    ) {
+
+                        // step 2. add a title Text composable
+                        Text(
+                            // step 3. Add a brief desc telling user how to complete form.
+                            text = "Add two hexadecimal characters between 0-9, A-F or a-f without the '#' for each channel"
+                        )
+
+
+                        /*
+                        step 4.
+
+                        Add 3 MutableState properties for each of the tree colors and
+                        another for a default color.
+                         */
+                        var RedChannel by remember { mutableStateOf("") }
+                        var GreenChannel by remember { mutableStateOf("") }
+                        var BlueChannel by remember { mutableStateOf("") }
+                        var colorToDisplay by remember { mutableStateOf(Color.White) }
+
+
+
+                        /*
+                        step 5.
+
+                        Add 3 material OutlinedTextField composables with lables of
+                        RedChannel, GreenChannel, BlueChannel, initialized with
+                        an empty string.
+                         */
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = RedChannel,
+                            onValueChange = { RedChannel = it },
+                            label = { Text("Red Channel") }
+                        )
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = GreenChannel,
+                            onValueChange = { GreenChannel = it },
+                            label = { Text("Green Channel") }
+                        )
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = BlueChannel,
+                            onValueChange = { BlueChannel = it },
+                            label = { Text("Blue Channel") }
+                        )
+
+                        // step 7. Add a button that takes the inputs from the color fields.
+                        Button(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                if (isValidHexInput(
+                                        RedChannel
+                                    ) && isValidHexInput(
+                                        GreenChannel
+                                    ) && isValidHexInput(
+                                        BlueChannel
+                                    )
+                                ) {
+                                    val colorString =
+                                        "#$RedChannel$GreenChannel$BlueChannel"
+                                    colorToDisplay =
+                                        Color(colorString.toColorInt())
+                                }
+                            }) {
+                            Text("CREATE COLOR")
+                        }
+                        Text(
+                            modifier = Modifier.background(colorToDisplay).padding(24.dp),
+                            text = "Created color display panel"
+                        )
+
+
+
+
+
+                    }
+
+
 
 
                 }
+
             }
         }
     }
 }
+
+/*
+step 6.
+
+Add a restriction function to each field to allow entry of only two hexadecimal characters.
+ */
+fun isValidHexInput(input: String): Boolean {
+    return input.filter {
+        it in '0'..'9' ||
+        it in 'A'..'F' ||
+        it in 'a'..'f'
+    }.length == 2
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
